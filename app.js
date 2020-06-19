d35fcf644454e5ffbb865af51e953286929a1ef1
unknown <ptah0414@naver.com>
2020-06-19 15:46:22 +0900

    #2.9 MVC Pattern part One

--- app.js
+++ app.js
@@ -3,12 +3,11 @@ import morgan from "morgan"; // application에서 발생하는 모든 일들을 
 import helmet from "helmet"; // node.js의 보안
 import cookieParser from "cookie-parser"; // session을 다루기 위해 쿠키에 유저 정보 저장
 import bodyParser from "body-parser";
-import { userRouter } from "./router"; // 라우터 받음
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
 const app = express();
-
-const handleHome = (req, res) => res.send("Hello from my home");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
 
 app.use(helmet());
 app.use(cookieParser());
@@ -16,10 +15,8 @@ app.use(bodyParser.json()); // 서버가 json을 이해할 수 있음
 app.use(bodyParser.urlencoded({ extended: true })); // 서버가 urlencoded를 이해할 수 있음
 app.use(morgan("dev"));
 
-app.get("/", handleHome);
-
-app.get("/profile", handleProfile);
-
+app.use("/", globalRouter);
 app.use("/user", userRouter); // use: 누군가 /use에 접속하면 userRouter의 라우터 전체를 사용
+app.use("/video", videoRouter);
 
 export default app;
---
+++ routers/globalRouter.js
@@ -0,0 +1,5 @@
+import express from "express";
+
+const globalRouter = express.Router(); // 라우터 생성
+
+export default globalRouter;
