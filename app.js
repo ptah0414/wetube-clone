37e058ae0fe20aa8060a658d072af1874c02ad76
unknown <ptah0414@naver.com>
2020-06-22 12:14:06 +0900

    2.16 Local Variables in Pug

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ import morgan from "morgan"; // application에서 발생하는 모든 일들을 
 import helmet from "helmet"; // node.js의 보안
 import cookieParser from "cookie-parser"; // session을 다루기 위해 쿠키에 유저 정보 저장
 import bodyParser from "body-parser";
+import { localsMiddleware } from "./views/partials/middlewares";
 import userRouter from "./routers/userRouter";
 import videoRouter from "./routers/videoRouter";
 import globalRouter from "./routers/globalRouter";
@@ -15,6 +16,7 @@ app.use(cookieParser());
 app.use(bodyParser.json()); // 서버가 json을 이해할 수 있음
 app.use(bodyParser.urlencoded({ extended: true })); // 서버가 urlencoded를 이해할 수 있음
 app.use(morgan("dev"));
+app.use(localsMiddleware);
 
 app.use(routes.home, globalRouter);
 app.use(routes.users, userRouter); // use: 누군가 /use에 접속하면 userRouter의 라우터 전체를 사용
