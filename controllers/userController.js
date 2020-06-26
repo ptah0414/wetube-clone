b10c80cb6b124a9ae39bd9b1964d8688cae0f241
unknown <ptah0414@naver.com>
2020-06-26 10:54:18 +0900

    2.25 More Controllers

--- controllers/userController.js
+++ controllers/userController.js
@@ -19,10 +19,15 @@ export const postJoin = (req, res) => {
   }
 };
 
-export const login = (req, res) => res.render("login", { pageTitle: "Log In" });
+export const getLogin = (req, res) =>
+  res.render("login", { pageTitle: "Log In" });
 
-export const logout = (req, res) =>
-  res.render("logout", { pageTitle: "Log Out" });
+export const postLogin = (req, res) => res.redirect(routes.home);
+
+export const logout = (req, res) => {
+  // To Do: Process Log Out
+  res.redirect(routes.home);
+};
 
 export const userDetail = (req, res) =>
   res.render("userDetail", { pageTitle: "User Detail" });
--- controllers/videoController.js
+++ controllers/videoController.js
@@ -1,4 +1,5 @@
 import { videos } from "../db";
+import routes from "../routes";
 
 export const home = (req, res) => {
   res.render("home", { pageTitle: "Home", videos }); // videos: videos와 같음
@@ -13,8 +14,16 @@ export const search = (req, res) => {
   res.render("search", { pageTitle: "Search", searchingBy, videos }); // searchingBy: searchingBy와 같음
 };
 
-export const upload = (req, res) =>
+export const getUpload = (req, res) =>
   res.render("upload", { pageTitle: "Upload" });
+
+export const postUpload = (req, res) => {
+  const {
+    body: { file, title, description }, // upload.pug의 form에서 전송한 data
+  } = req;
+  // To Do: Upload and save video
+  res.redirect(routes.videoDetail(324393)); // dummy id
+};
 
 export const videoDetail = (req, res) =>
   res.render("videoDetail", { pageTitle: "Video Detail" });
