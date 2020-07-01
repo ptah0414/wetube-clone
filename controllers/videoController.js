f321d407cd8785dbecf5afc51d87320127089667
unknown <ptah0414@naver.com>
2020-07-01 16:14:27 +0900

    3.8 Getting Video by ID

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -37,8 +37,17 @@ export const postUpload = async (req, res) => {
   res.redirect(routes.videoDetail(newVideo.id)); // dummy id
 };
 
-export const videoDetail = (req, res) =>
-  res.render("videoDetail", { pageTitle: "Video Detail" });
+export const videoDetail = async (req, res) => {
+  const {
+    params: { id },
+  } = req;
+  try {
+    const video = await Video.findById(id);
+    res.render("videoDetail", { pageTitle: "Video Detail", video }); //video 변수를 template에 전달
+  } catch (error) {
+    res.redirect(routes.home);
+  }
+};
 
 export const editVideo = (req, res) =>
   res.render("editVideo", { pageTitle: "Edit Video" });
