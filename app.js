cd32ea0e5d6c6bbf43c822b58e7c2ad825594393
unknown <ptah0414@naver.com>
2020-07-09 21:06:58 +0900

    #6.4 Sessions on Express

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ import helmet from "helmet"; // node.js의 보안
 import cookieParser from "cookie-parser"; // session을 다루기 위해 쿠키에 유저 정보 저장
 import bodyParser from "body-parser";
 import passport from "passport";
+import session from "express-session";
 import { localsMiddleware } from "./middlewares";
 import routes from "./routes";
 import userRouter from "./routers/userRouter";
@@ -24,6 +25,13 @@ app.use(cookieParser());
 app.use(bodyParser.json()); // 서버가 json을 이해할 수 있음
 app.use(bodyParser.urlencoded({ extended: true })); // 서버가 urlencoded를 이해할 수 있음
 app.use(morgan("dev"));
+app.use(
+  session({
+    secret: process.env.COOKIE_SECRET,
+    resave: true,
+    saveUninitialized: false,
+  })
+);
 app.use(passport.initialize()); // passport 초기화
 app.use(passport.session()); // session 저장. session이 가진 쿠키 이용
 
