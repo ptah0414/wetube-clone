0f2d49f77433554ac22f67f4a37f61c864d9ff12
unknown <ptah0414@naver.com>
2020-07-09 14:39:01 +0900

    #5.1 Footer, Login & Join

--- middlewares.js
+++ middlewares.js
@@ -8,7 +8,7 @@ export const localsMiddleware = (req, res, next) => {
   res.locals.routes = routes;
   res.locals.user = {
     // 존재하지 않는 가짜 정보
-    isAuthenticated: true,
+    isAuthenticated: false,
     id: 1,
   };
   next();
